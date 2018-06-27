fc9dc987241753892782b21c71d83a8d90d87e58
JohnyLi <491245006@qq.com>
2018-06-27 22:50:56 +0800

    2333

---
+++ Listener.py
@@ -0,0 +1,320 @@
+from flask import Flask, request, session,  redirect, url_for, abort, \
+     render_template, flash,make_response,jsonify,json
+from flask_cors import CORS
+from urllib import request as api,parse
+import time
+import Caculate
+app=Flask(__name__)
+app.config.update(dict(DEBUG=True, SECRET_KEY='Chuangxin!!!!!233', ))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+CORS(app, supports_credentials=True)
+ip="localhost"
+port=23333
+pageoffset=9
+firstpage=1
+maxshow=5
+url_login="http://%s:%s/login" %(ip,port)
+url_data = "http://%s:%s/query/data"% (ip,port)
+url_month= "http://%s:%s/query/month"% (ip,port)
+url_change_history="http://%s:%s/query/change"% (ip,port)
+url_daka='http://%s:%s/daka/' %(ip,port)
+url_bumen='http://%s:%s/bumen' %(ip,port)
+url_information='http://%s:%s/information' %(ip,port)
+url_change_information='http://%s:%s/information/change' %(ip,port)
+url_change_password='http://%s:%s/information/password'%(ip,port)
+url_banci_get='http://%s:%s/banci/get'%(ip,port)
+url_banci_change='http://%s:%s/banci/change'%(ip,port)
+
+sidebar=[{'name':'今日打卡详情','href':'/today'},{'name':'打卡历史记录','href':'/history'},
+         {'name': '员工信息管理', 'href': '/information'},{'name': '班次设置', 'href': '/banci'}]
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        data={'username':username,'password':password}
+        result=GetData(url_login,data)
+
+        try:
+            result=json.loads(result)
+            privilege=int(result['privilege'])
+            if(privilege==0):
+                session.clear()
+                error='用户名或密码错误'
+                return render_template('login.html', error=error)
+            elif(privilege  ==  1):
+                session['login'] = True
+                session['username'] = username
+                session['privilege'] = privilege
+                return redirect(url_for('daka'))
+            elif (privilege == 2):
+                session['login']=True
+                session['username']=username
+                session['privilege']=privilege
+                return redirect(url_for('today'))
+
+
+        except:
+            print("load json error")
+            error = "服务器出现错误"
+            session.clear()
+            return render_template('login.html', error=error)
+
+    if(session.get('login')) and session.get('username') and session.get('privilege'):
+        if session['privilege']==1:
+            return redirect(url_for('daka'))
+        elif session['privilege']==2:
+            return redirect(url_for('today'))
+
+    return  render_template('login.html', error=error)
+
+@app.route('/daka', methods=['GET', 'POST'])
+def daka():
+    url = url_daka
+
+    if request.method == 'POST':
+        try:
+            myjson=json.loads(request.get_data())
+            username=myjson['username']
+            banci=myjson['banci']
+            data={'username':username,'banci':banci}
+            myurl=url+'now'
+            result=GetData(myurl,data)
+            return result
+        except:
+            return jsonify({'status':'Failed'})
+
+    if(session.get('login') and session.get('username') and session.get('privilege') ):
+        if(session['privilege']==2):
+            return redirect(url_for('Today'))
+    else:
+        session.clear()
+        return redirect(url_for('login'))
+
+    myurl=url+'banci'
+    username=session['username']
+    data={'username':username}
+    result=GetData(myurl,data)
+    try:
+        mydict=json.loads(result)
+        banci=mydict['banci']
+        status=mydict['status']
+        if(banci!='none'):
+            if(status=='unfinished'):
+                return render_template('daka.html', name=username, banci=banci)
+            elif(status=='finished'):
+                return render_template('daka.html', name=username, banci=banci,button="打卡完成")
+        else:
+            return render_template('daka.html', name=username, banci=banci, button="无需打卡")
+    except:
+        return redirect(url_for("Error"))
+
+
+@app.route('/today', methods=['GET', 'POST'])
+def today():
+    if(session.get('login') and session.get('username') and session.get('privilege') ):
+        if(session['privilege']==1):
+            return redirect(url_for('daka'))
+    else:
+        session.clear()
+        return redirect(url_for('login'))
+    username=session['username']
+
+    page=int(request.args.get("p",1))
+
+    date=time.strftime("%Y.%m.%d")
+    data = {"date": date}
+    result = GetData(url_data, data)
+    result=json.loads(result)
+    data = result['data']
+    mydata = Caculate.GetPageDict(page,data,pageoffset,maxshow)
+    return render_template('Today.html', data=mydata,username=username,sidebar=sidebar,active='今日打卡详情')
+
+@app.route('/history', methods=['GET', 'POST'])
+def history():
+    if (session.get('login') and session.get('username') and session.get('privilege')):
+        if (session['privilege'] == 1):
+            return redirect(url_for('daka'))
+    else:
+        session.clear()
+        return redirect(url_for('login'))
+    username = session['username']
+
+    page = int(request.args.get("p", 1))
+    date = request.args.get("date", time.strftime("%Y.%m.%d"))
+
+    data={'query':'1'}
+    result = GetData(url_month, data)
+    result = json.loads(result)
+    data=result['month']
+    start=Caculate.getstartmonth(data)
+
+    data = {"date": date}
+    result = GetData(url_data, data)
+    result = json.loads(result)
+    data=result['data']
+    mydata = Caculate.GetPageDict(page, data, pageoffset-1, maxshow)
+
+    return render_template('History.html', data=mydata,date=date,start=start,username=username,sidebar=sidebar,active='打卡历史记录')
+
+
+@app.route('/change/history', methods=['POST'])
+def change_history():
+
+    if request.method == 'POST':
+        if (session.get('login') and session.get('username') and session.get('privilege')):
+            pass
+        else:
+            return jsonify({'status': '请关闭浏览器重新登录'})
+        try:
+            myjson=json.loads(request.get_data())
+            userid=myjson['id']
+            username=myjson['name']
+            banci=myjson['banci']
+            time=myjson['time']
+            beizhu=myjson['beizhu']
+            caozuo=myjson['caozuo']
+            data={'userid':userid,'username':username,'banci':banci,'beizhu':beizhu,'caozuo':caozuo,'time':time}
+            result=GetData(url_change_history,data)
+            return result
+        except:
+            return jsonify({'status':'Failed'})
+
+
+@app.route('/information', methods=['GET','POST'])
+def information():
+    if request.method == 'POST':
+        try:
+            if (session.get('login') and session.get('username') and session.get('privilege')):
+                pass
+            else:
+                return jsonify({'status': '请关闭浏览器重新登录'})
+            myjson=json.loads(request.get_data())
+            userid=myjson['id']
+            username=myjson['name']
+            bumen=myjson['bumen']
+            beizhu=myjson['beizhu']
+            caozuo=myjson['caozuo']
+            if not myjson.__contains__('password'):
+                password=""
+            else:
+                password=myjson['password']
+            data={'userid':userid,'username':username,'bumen':bumen,'beizhu':beizhu,'caozuo':caozuo,'password':password}
+            result=GetData(url_change_information,data)
+            return result
+        except:
+            return jsonify({'status':'Failed'})
+
+    if (session.get('login') and session.get('username') and session.get('privilege')):
+        if (session['privilege'] == 1):
+            return redirect(url_for('daka'))
+    else:
+        session.clear()
+        return redirect(url_for('login'))
+    username = session['username']
+
+    data={'query':'1'}
+    result = GetData(url_bumen, data)
+    result = json.loads(result)
+    allbumen = result['bumen']
+    firstbumen=allbumen[0]
+
+    page = int(request.args.get("p", 1))
+    nowbumen = request.args.get("bumen",firstbumen)
+    data={'bumen':nowbumen}
+    result = GetData(url_information, data)
+    result = json.loads(result)
+    data = result['information']
+    mydata = Caculate.GetPageDict(page, data, pageoffset - 2, maxshow)
+    return render_template('Information.html', data=mydata, username=username,nowbumen=nowbumen,allbumen=allbumen,sidebar=sidebar,active="员工信息管理")
+
+
+@app.route('/myinformation', methods=['GET','POST'])
+def myinformation():
+    if request.method == 'POST':
+        if (session.get('login') and session.get('username') and session.get('privilege')):
+            pass
+        else:
+            return jsonify({'status': '请关闭浏览器重新登录'})
+        myjson = json.loads(request.get_data())
+        username=myjson['username']
+        oldpassword=myjson['oldpassword']
+        newpassword=myjson['newpassword']
+        data={'username':username,'oldpassword':oldpassword,'newpassword':newpassword}
+        result = GetData(url_change_password, data)
+        return result
+
+    if (session.get('login') and session.get('username') and session.get('privilege')):
+        if (session['privilege'] == 1):
+            return redirect(url_for('daka'))
+    else:
+        session.clear()
+        return redirect(url_for('login'))
+    username = session['username']
+
+    return render_template('My_Information.html', username=username,sidebar=sidebar)
+
+@app.route('/banci', methods=['GET','POST'])
+def banci():
+    if request.method == 'POST':
+        if (session.get('login') and session.get('username') and session.get('privilege')):
+            pass
+        else:
+            return jsonify({'status': '请关闭浏览器重新登录'})
+        myjson = json.loads(request.get_data())
+        result = GetData(url_banci_change, myjson)
+        return result
+
+
+
+    if (session.get('login') and session.get('username') and session.get('privilege')):
+        if (session['privilege'] == 1):
+            return redirect(url_for('daka'))
+    else:
+        session.clear()
+        return redirect(url_for('login'))
+    username = session['username']
+
+    data={'query':'1'}
+    result=GetData(url_banci_get,data)
+    result=json.loads(result)
+    allbanci=result['banci']
+
+    page = int(request.args.get("p", 1))
+    mydata = Caculate.GetPageDict(page, allbanci, pageoffset - 1, maxshow)
+    return render_template('Banci.html', data=mydata, username=username,sidebar=sidebar,active='班次设置')
+
+
+
+
+@app.route('/out')
+def out():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/Error')
+def Error():
+    return "服务器出错，请稍后再试"
+
+def GetData(url,data=None):
+    if(data!=None):
+        Nowdata=json.dumps(data)
+        Nowdata=Nowdata.encode('utf-8')
+        NowRequest = api.Request(url)
+        f=api.urlopen(NowRequest,Nowdata)
+    else:
+        NowRequest = api.Request(url)
+        f = api.urlopen(NowRequest)
+    return f.read().decode('utf-8')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80)
---
+++ SuperTest.py
@@ -0,0 +1,495 @@
+from urllib import request,error
+import json
+import socket
+import time
+import threading
+
+########=========================================参数设置=============================##################
+
+ip="localhost"
+port=23333
+
+timeout=5
+report={}
+mutex=threading.Lock()
+
+Error="Error"
+Failed="Failed"
+Successed="Successed"
+
+########=========================================参数设置=============================##################
+
+socket.setdefaulttimeout(timeout)
+print("#############################################################################")
+
+nowtime=time.time()
+
+def go_url(url,data):
+    try:
+        start=time.time()
+        rq = request.Request(url)
+        dic = json.dumps(data)
+        mydata = dic.encode('utf-8')
+        fopen = request.urlopen(rq, mydata)
+    except error.HTTPError as e:
+        print("Error:HTTPError")
+        print(e.reason)
+        print("请查看服务器端的错误")
+        return Error
+    except error.URLError as e:
+        print("Error:URLError")
+        print(e.reason)
+        print("目测是端口或者ip不正确")
+        return Error
+    except socket.timeout:
+        print("Error:timeout")
+        print("服务器超时，当前设置的timeout为%d秒"%timeout)
+        return Error
+
+    try:
+        print("访问时间:%f"%(time.time()-start))
+        result = fopen.read().decode('utf-8')
+        result = json.loads(result)
+        return result
+
+    except json.decoder.JSONDecodeError as e:
+        print("Error:JSONDecodeError")
+        print("返回的json无法加载")
+        return Error
+    except UnboundLocalError:
+        print("访问失败")
+        return Error
+
+
+def MakeResult(word,testname,data=None):
+    global mutex
+    if(data==None):
+        print(word)
+        report[testname]=word
+    else:
+        print("%s result:" % testname)
+        print(json.dumps(data))
+        print(testname + " " + Successed)
+        report[testname] = Successed
+    print("#############################################################################")
+    mutex.release()
+
+def PrintReport():
+    print("=============================================================================")
+    count = 0
+    print("Report:")
+    for key in report:
+        print(key + ":" + report[key])
+        if (report[key] == Successed):
+            count += 1
+    print("Pass: " + str(count) + "/" + str(len(report)))
+    print("=============================================================================")
+
+def CheckDict(mydict,key):
+    for i in key:
+        if not mydict.__contains__(i):
+            return i
+    return True
+
+###########=============上面是静态方法==============================##########
+###########=============下面是各种test==============================##########
+
+#test for url "/login"
+def test_login(username,password):
+    testname="test_login"
+    url="http://%s:%s/login"%(ip,port)
+    data={'username':username,'password':password}
+    result=go_url(url,data)
+
+    global mutex
+    mutex.acquire()
+
+    print(testname + ":")
+    if(result==Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            privilege=result["privilege"]
+        except KeyError:
+            print("json中无privilege的key")
+            print("json:"+json.dumps(result))
+            MakeResult(Failed,testname)
+            return
+
+        MakeResult(Successed,testname,result)
+
+#test for url "/daka/banci"
+def test_daka_banci(username):
+    testname="test_daka_banci"
+    url = "http://%s:%s/daka/banci" % (ip, port)
+    data={"username":username}
+    result=go_url(url,data)
+
+    global mutex
+    mutex.acquire()
+
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        seek=False
+        try:
+            banci=result["banci"]
+            if(banci!="none"):
+                seek=True
+        except KeyError:
+            print("json中无banci的key")
+            print("json:"+json.dumps(result))
+            MakeResult(Failed,testname)
+            return
+        try:
+            if(seek):
+                status=result["status"]
+                if(status=="finished" or status=="unfinished"):
+                    MakeResult(Successed, testname, result)
+                else:
+                    print("status应是finished或unfinished")
+                    print("json:" + json.dumps(result))
+                    MakeResult(Failed, testname)
+        except KeyError:
+            print("json中无status的key")
+            print("json:"+json.dumps(result))
+            MakeResult(Failed,testname)
+            return
+
+#test for url "/daka/now"
+def test_daka_now(username,banci):
+    testname = "test_daka_now"
+    url = "http://%s:%s/daka/now" % (ip, port)
+    data = {"username": username,"banci":banci}
+    result = go_url(url, data)
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            status=result["status"]
+        except KeyError:
+            print("json中无status的key")
+            print("json:"+json.dumps(result))
+            MakeResult(Failed,testname)
+            return
+
+        MakeResult(Successed,testname,result)
+
+
+#test for url "/query/month"
+def test_query_month():
+    testname="test_query_month"
+    url = "http://%s:%s/query/month" % (ip, port)
+    data={"query":"1"}
+    result = go_url(url, data)
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            month=result["month"]
+            if(type(month)==type([])):
+                for i in month:
+                    array=i.split('.')
+                    if(len(array)!=2) or not array[0].isdigit() or not array[1].isdigit():
+                        print("此月份格式不对:",end=i+"\n")
+                        MakeResult(Failed, testname)
+                        return
+            else:
+                print("month的值应为list格式")
+                MakeResult(Failed, testname)
+                return
+
+        except KeyError:
+            print("json中无month的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+
+#test for url "/query/data"
+def test_query_data(date):
+    testname = "test_query_data"
+    url = "http://%s:%s/query/data" % (ip, port)
+    data = {"date": date}
+    result = go_url(url, data)
+
+    mykey=["userid","username","time","banci"]
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            data=result['data']
+            if (type(data) == type([])):
+                for i in data:
+                    Check=CheckDict(i,mykey)
+                    if not (Check):
+                        print("data中无%s的key"% Check)
+                        print("json:" + json.dumps(result))
+                        MakeResult(Failed, testname)
+                        return
+
+            else:
+                print(type(data))
+                print("data的值应为list格式")
+                MakeResult(Failed, testname)
+                return
+
+
+        except KeyError:
+            print("json中无data的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+#test for url "/query/change"
+def test_query_change(userid,username,time,banci,beizhu,caozuo):
+    testname = "test_query_change"
+    url = "http://%s:%s/query/change" % (ip, port)
+    data = {"userid": userid,"username":username,"time":time,"banci":banci,'beizhu':beizhu,"caozuo":caozuo}
+    result = go_url(url, data)
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            status=result['status']
+        except KeyError:
+            print("json中无status的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+#test for url "/information/password"
+def test_information_password(username,oldpassword,newpassword):
+    testname="test_information_password"
+    url = "http://%s:%s/information/password" % (ip, port)
+    data={"username":username,"oldpassword":oldpassword,"newpassword":newpassword}
+    result = go_url(url, data)
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            status=result['status']
+        except KeyError:
+            print("json中无status的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+#test for url "/bumen"
+def test_bumen():
+    testname = "test_bumen"
+    url = "http://%s:%s/bumen" % (ip, port)
+    data={"query":"1"}
+    result=go_url(url, data)
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            bumen=result['bumen']
+            if(type(bumen)==type([])):
+                if(len(bumen)<1):
+                    print("无部门值")
+                    MakeResult(Failed, testname)
+                    return
+
+
+            else:
+                print("bumen的值应为list格式")
+                MakeResult(Failed, testname)
+                return
+
+        except KeyError:
+            print("json中无bumen的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+#test for url "/information"
+def test_information(bumen):
+    testname = "test_information"
+    url = "http://%s:%s/information" % (ip, port)
+    data = {"bumen":bumen}
+    result = go_url(url, data)
+    mykey=['userid','username','bumen']
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            information=result['information']
+            if(type(information)==type([])):
+                for i in information:
+                    Check=CheckDict(information,mykey)
+                    if not (Check):
+                        print("data中无%s的key" % Check)
+                        print("json:" + json.dumps(result))
+                        MakeResult(Failed, testname)
+                        return
+
+            else:
+                print("information的值应为list格式")
+                MakeResult(Failed, testname)
+                return
+
+        except KeyError:
+            print("json中无information的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+#test for url "/information/change"
+def test_information_change(userid,username,password,bumen,beizhu):
+    testname = "test_information_change"
+    url = "http://%s:%s/information/change" % (ip, port)
+    data = {"userid": userid, "username": username, "password": password, "bumen": bumen,  'beizhu': beizhu}
+    result = go_url(url, data)
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            status=result['status']
+        except KeyError:
+            print("json中无status的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+
+#test for url "/banci/get"
+def test_banci_get():
+    testname = "test_banci_get"
+    url = "http://%s:%s/banci/get" % (ip, port)
+    data={"query":1}
+    result = go_url(url, data)
+
+    mykey=["username","bumen",'start','end','interval']
+
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            banci=result['banci']
+            if(type(banci)==type([])):
+                for i in banci:
+                    Check=CheckDict(banci,mykey)
+                    if not(Check):
+                        print("data中无%s的key" % Check)
+                        print("json:" + json.dumps(result))
+                        MakeResult(Failed, testname)
+                        return
+            else:
+                print("banci的值应为list格式")
+                MakeResult(Failed, testname)
+                return
+
+        except KeyError:
+            print("json中无banci的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+
+#test for url "/banci/change"
+def test_banci_change(username,bumen,start,end,interval,caozuo):
+    testname = "test_banci_change"
+    url = "http://%s:%s/banci/change" % (ip, port)
+    data = {"username": username, "bumen": bumen, 'start': start, 'end': end, 'interval': str(interval),'caozuo':caozuo}
+    result = go_url(url, data)
+    global mutex
+    mutex.acquire()
+    print(testname + ":")
+    if (result == Error):
+        MakeResult(Failed, testname)
+    else:
+        try:
+            status = result['status']
+        except KeyError:
+            print("json中无status的key")
+            print("json:" + json.dumps(result))
+            MakeResult(Failed, testname)
+            return
+        MakeResult(Successed, testname, result)
+
+
+if __name__ == '__main__':
+    TestList=[]
+
+    TestList.append(threading.Thread(target=test_login, args=("jack", "123456")))
+    #test_login(username,password)  用于测试"/login"
+    TestList.append(threading.Thread(target=test_daka_banci, args=("jack",)) )
+    #test_daka_banci(username)     用于测试"/daka/banci"
+    TestList.append(threading.Thread(target=test_daka_now, args=("jack","2018.5.30 9:00 签到")))
+    #test_daka_now(username,banci)     用于测试"/daka/now"
+    TestList.append(threading.Thread(target=test_query_month) )
+    #test_query_month()     用于测试"/query/month"
+    TestList.append(threading.Thread(target=test_query_data,args=("2018.6.3",)  ) )
+    #test_query_data(date)     用于测试"/query/data"
+    TestList.append(threading.Thread(target=test_query_change, args=("110","jack","2018.5.31 8:00","2018.5.31 20:30 签退","无","delete") ) )
+    # test_query_change(userid,username,time,banci,beizhu,caozuo)     用于测试"/query/change"
+    TestList.append(threading.Thread(target=test_information_password,args=("jack","123456","234567")))
+    # test_information_password(username,oldpassword,newpassword)     用于测试"/information/password"
+    TestList.append(threading.Thread(target=test_bumen))
+    # test_bumen()     用于测试"/bumen"
+    TestList.append(threading.Thread(target=test_information,args=("清洁部",)))
+    # test_information(bumen)   用于测试"/information"
+    TestList.append(threading.Thread(target=test_information_change, args=("110","jack","123456","清洁部","无")))
+    # test_information_change(userid,username,password,bumen,beizhu)   用于测试"/information/change"
+    TestList.append(threading.Thread(target=test_banci_get))
+    # test_banci_get() 用于测试"/banci/get"
+    TestList.append(threading.Thread(target=test_banci_change, args=("admin", "清洁部", "2018.5.30 9:00", "2018.5.30 20:00", 15,'change')))
+    # test_banci_change(username,bumen,start,end,interval,caozuo) 用于测试"/banci/change"
+
+
+
+    for thread in TestList:
+        thread.start()
+
+    for thread in TestList:
+        thread.join()
+
+    PrintReport()
+
+    print("总花费时间:"+str(time.time()-nowtime)+"秒")
